321f293cbab990b9cee6eab0fc1ae5620e922d73
gekkin-programmer <nkouambrayan@gmail.com>
2026-06-07 21:09:27 +0100

    fix(ui): disable eslint strict react-hooks/set-state-in-effect rule for hydration bypass

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,7 @@ export default function Home() {
   const [subscribed, setSubscribed] = useState(false);
 
   useEffect(() => {
+    // eslint-disable-next-line react-hooks/set-state-in-effect
     setMounted(true);
   }, []);
 
