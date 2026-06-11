a485b9717b77a96211d8d3316cdc501a150324a8
gekkin-programmer <nkouambrayan@gmail.com>
2026-06-11 16:45:12 +0100

    feat: complete A Propos and FAQ sections

--- frontend-next/src/app/page.tsx
+++ frontend-next/src/app/page.tsx
@@ -10,6 +10,8 @@ import ImpactSection from '@/components/ImpactSection';
 import PublishSection from '@/components/PublishSection';
 import UsersSection from '@/components/UsersSection';
 import ConnectSection from '@/components/ConnectSection';
+import AProposSection from '@/components/AProposSection';
+import FaqSection from '@/components/FaqSection';
 
 export default function Home() {
   return (
@@ -73,6 +75,8 @@ export default function Home() {
       <PublishSection />
       <ConnectSection />
       <UsersSection />
+      <AProposSection />
+      <FaqSection />
     </main>
   );
 }
